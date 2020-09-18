1db40b176c50cb4ac12ac51027d4b1ae0b3c9748
ankush789 <ankushsharma.4771@gmail.com>
2020-09-19 00:01:21 +0530

    install ejs and setup view engine

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 8000;
 
 app.use('/', require('./routes'));
 
+//install ejs and setup view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running server: ${err}`);
