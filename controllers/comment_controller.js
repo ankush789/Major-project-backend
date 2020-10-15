344f28dc6a424e987a1e47d5e39e5e88d1b48387
ankush789 <ankushsharma.4771@gmail.com>
2020-10-15 19:05:41 +0530

    Deleting a Comment by Post User or comment creator

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -21,3 +21,29 @@ module.exports.createComment = function(req,res){
         }
     });
 }
+
+module.exports.deleteComment = function(req,res){
+    Comment.findById(req.params.id, function(err, comment){
+        
+        //Pre-populating the post in the Comment Schema to get post's user
+           Comment.find({ _id: req.params.id }).populate('post').exec(function(err, Comment){
+
+               //postUser contains the user who created the post
+                let postUser = Comment[0].post.user;
+
+                //Only person who made the comment or the person on whom post comment is made can delete the comment
+               if(comment.user == req.user.id || postUser == req.user.id ){
+                    let postId = comment.post;
+                    comment.remove();
+
+                //Deleting comment ids from the post.comments array 
+                    Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}},function(err, post){
+                        return res.redirect('back')
+                    })
+                }
+                else{
+                    return res.redirect('back');
+                }
+            })
+    })
+}
--- routes/comments.js
+++ routes/comments.js
@@ -4,6 +4,6 @@ const passport = require('passport');
 const commentController = require('../controllers/comment_controller');
 
 router.post('/create-comment', passport.checkAuthentication, commentController.createComment);
-
+router.get('/delete/:id', passport.checkAuthentication, commentController.deleteComment)
 
 module.exports = router;
