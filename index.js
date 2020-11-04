0bce7955327e5fff5fc2af34722cc2cbddb94dc3
ankush789 <ankushsharma.4771@gmail.com>
2020-11-04 08:51:03 +0530

    Installing Multer and adding input to upload file in form

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'})
 const cookieParser = require('cookie-parser')
 const app = express();
 const port = 9000;
