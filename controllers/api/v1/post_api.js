0e5f8310f8272e3bdbac3100b2186e0b9818c158
ankush789 <ankushsharma.4771@gmail.com>
2020-11-06 17:04:31 +0530

    Displaying all posts in API and deleting using post id

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -1,7 +1,43 @@
+const Post = require('../../../models/post')
+const Comment = require('../../../models/comment');
+
 //index is used when you want to list down something
-module.exports.index = function(req,res){
+module.exports.index = async function(req,res){
+ let posts = await Post.find({})
+    .sort('-createdAt') //Sort post according to the time of creation
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    });
+
     return res.json(200,{
         message: "List of posts",
-        posts: []
+        posts: posts
     })
+}
+
+//Deleting Posts from the API
+module.exports.destroy = async function(req,res){
+    try {
+         //Find the post having the id passed in URL
+        let post =  await Post.findById(req.params.id);
+            post.remove();
+            //Delete all those comments which have post: req.params.id
+            await Comment.deleteMany({post: req.params.id });
+
+            return res.json(200, {
+                message: "Post and associated comments deleted"
+            })
+        
+        // else{
+        //     return res.redirect('back');
+        // }
+    } catch (error) {
+        return res.json(500, {
+            message: "Internal Server Error"
+        });
+    }
 }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const postsApi = require('../../../controllers/api/v1/post_api');
 
 router.get('/',postsApi.index);
-
+router.delete('/:id', postsApi.destroy);
 
 module.exports = router
 
