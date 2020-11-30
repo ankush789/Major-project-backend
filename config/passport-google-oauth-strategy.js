60ccc2b4bf53ecdf673d40ee7bf8b65a673f6885
ankush789 <ankushsharma.4771@gmail.com>
2020-11-30 15:57:31 +0530

    UPDATED DESIGN

--- config/passport-google-oauth-strategy.js
+++ config/passport-google-oauth-strategy.js
@@ -25,6 +25,7 @@ passport.use(new googleStrategy({
                 User.create({
                     name: profile.displayName,
                     email: profile.emails[0].value,
+                    avatar: profile.photos[0].value,
                     password: crypto.randomBytes(20).toString('hex')
                 }, function(err,user){
                     if(err){console.log("error in creating new user using google strategy",err);return;}
